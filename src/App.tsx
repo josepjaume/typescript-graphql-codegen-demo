75cc10c7c09237551555a55ec5bd0819528d171c
Josep Jaume Rey <josepjaume@codegram.com>
2021-11-21 18:20:00 +0000

    Live coding

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,21 @@
-import {
-  SimpleGrid,
-  Input,
-  Box,
-  Flex,
-  Container,
-  Spinner,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  ListItem,
-  PopoverBody,
-  PopoverHeader,
-  List,
-  PopoverArrow,
-  InputGroup,
-  InputLeftElement,
-} from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
-
-import { useRef, useState } from "react";
+import { Container, SimpleGrid, Box, Image } from "@chakra-ui/react";
 import { useGetCharactersQuery } from "./graphql";
 
 function App() {
-  let [search, setSearch] = useState("");
-
-  let searchRef = useRef<HTMLInputElement>(null);
-
-  let { data, loading } = useGetCharactersQuery({
-    variables: {
-      search,
-    },
-  });
+  let { data } = useGetCharactersQuery();
 
   return (
     <Container maxW="container.xl" marginY="4">
-      <Box marginBottom={4}>
-        <form
-          onSubmit={(event) => {
-            setSearch(searchRef?.current?.value || "");
-            event.preventDefault();
-          }}
-        >
-          <InputGroup>
-            <InputLeftElement
-              pointerEvents="none"
-              children={<SearchIcon color="gray.300" />}
-            />
-            <Input ref={searchRef} placeholder="Search..." />
-          </InputGroup>
-        </form>
-      </Box>
-
-      {loading ? (
-        <Spinner />
-      ) : (
-        <SimpleGrid minChildWidth="12rem" spacing={4}>
-          {data?.characters?.results?.map((character) => (
-            <Flex align="center" flexDirection="column">
-              <Popover trigger="hover">
-                <PopoverTrigger>
-                  {character?.image && (
-                    <img
-                      src={character?.image}
-                      alt={character.name || "A Rick & Morty's character"}
-                    />
-                  )}
-                </PopoverTrigger>
-                <PopoverContent>
-                  <PopoverArrow />
-                  <PopoverHeader>
-                    <strong>{character?.name}</strong>,{" "}
-                    {character?.episode.length} episodes
-                  </PopoverHeader>
-                  <PopoverBody>
-                    <List>
-                      <ListItem>
-                        {character?.species} ({character?.gender})
-                      </ListItem>
-                      <ListItem>
-                        {character?.origin?.name} (
-                        {character?.origin?.dimension})
-                      </ListItem>
-                      {character?.type && (
-                        <ListItem>{character?.type}</ListItem>
-                      )}
-                    </List>
-                  </PopoverBody>
-                </PopoverContent>
-              </Popover>
-              <Box marginTop={2}>
-                <strong>{character?.name}</strong>
-              </Box>
-            </Flex>
-          ))}
-        </SimpleGrid>
-      )}
+      <SimpleGrid minChildWidth="10em">
+        {data?.characters?.results?.map((character) => (
+          <Box>
+            {character?.image && <Image src={character?.image} />}
+            <div>
+              <strong>{character?.name}</strong>
+            </div>
+          </Box>
+        ))}
+      </SimpleGrid>
     </Container>
   );
 }
